47ab5f33fc2cdc38bb73d5d919a0e36de324c58a
DESKTOP-PCDMJK8\E7450 <galihsahar21@gmail.com>
2022-04-08 20:50:25 +0700

    Changing alert and confirmation button into SweetAlert

--- department.js
+++ department.js
@@ -13,20 +13,20 @@ function loadContent() {
       var result = JSON.parse(this.responseText);
       var data = result.data;
       document.getElementById("hasil-department").innerHTML = `<thead><tr>
-                    <th class="table-info text-center"">Department Id</th>
-                    <th class="table-info text-center"">Department Name</th>
-                    <th class="table-info text-center"">Location Id</th>
-                    <th class="table-info text-center"">Delete</th>
-                    <th class="table-info text-center"">Edit</th></tr></thead>`;
+                    <th class="table-danger text-center">Department Id</th>
+                    <th class="table-danger text-center">Department Name</th>
+                    <th class="table-danger text-center">Location Id</th>
+                    <th class="table-danger text-center">Delete</th>
+                    <th class="table-danger text-center">Edit</th></tr></thead>`;
       for (let i = 0; i < data.length; i++) {
         const element = data[i];
         document.getElementById(
           "hasil-department"
-        ).innerHTML += `<tr><td class="table-secondary text-center"">${element.departmentId}</td>
-                            <td class="table-secondary text-center"">${element.departmentName}</td>
-                            <td class ="table-secondary text-center"">${element.locationId}</td>
-                            <td class="table-secondary  text-center""><button class="btn btn-dark text-center" type="submit" onclick="deleteById(${data[i].departmentId})"><i class="fa-solid fa-trash-can"></i></button>
-                            <td class="table-secondary text-center""><a class="btn btn-success" type="submit" value="Edit" href="update.html?id=${data[i].departmentId}">Edit</a></td></tr>`;
+        ).innerHTML += `<tr class="text-center"><td class="table-secondary text-center py-4">${element.departmentId}</td>
+                            <td class="table-secondary text-center py-4">${element.departmentName}</td>
+                            <td class ="table-secondary text-center py-4">${element.locationId}</td>
+                            <td class="table-secondary  text-center"><button class="btn btn-warning text-light text-center" type="submit" onclick="deleteById(${data[i].departmentId})"><i class="fa-solid fa-trash-can fa-2x"></i></button>
+                            <td class="table-secondary text-center"><a class="text-success" type="submit" href="update.html?id=${data[i].departmentId}"><i class="fa-solid fa-circle-plus fa-3x"></i></a></td></tr>`;
       }
 
       document.getElementById("button").innerHTML = "Done";
@@ -44,13 +44,38 @@ function clearResult() {
 }
 
 function deleteById(i) {
-  var xhr = new XMLHttpRequest();
-  var url = `http://localhost:9091/departments/${[i]}`;
-  xhr.open("DELETE", url, true);
-  xhr.send();
-  xhr.onloadend = function () {
-    loadContent();
-  };
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      var xhr = new XMLHttpRequest();
+      var url = `http://localhost:9091/departments/${[i]}`;
+      xhr.open("DELETE", url, true);
+      xhr.onload = function () {
+        Swal.fire(
+          'Delete!',
+          'Data has been deleted!',
+          'success'
+        );
+        console.log(this.responseText);
+      }
+      xhr.onloadend = function () {
+        Swal.fire(
+          'Delete!',
+          'You clicked the button!',
+          'success'
+        );
+        loadContent();
+      };
+      xhr.send();
+    }
+  });
 }
 
 function searchDepartmentByFirstName(name) {
@@ -72,11 +97,11 @@ function searchDepartmentByFirstName(name) {
         const element = data[i];
         document.getElementById(
           "hasil-department"
-        ).innerHTML += `<tr><td class="table-secondary text-center"">${element.departmentId}</td>
-                            <td class="table-secondary text-center"">${element.departmentName}</td>
-                            <td class ="table-secondary text-center"">${element.locationId}</td>
-                            <td class="table-secondary  text-center""><button class="btn btn-warning text-center" type="submit" onclick="deleteById(${data[i].departmentId})"><i class="fa-solid fa-trash-can"></i></button>
-                            <td class="table-secondary text-center""><a class="btn btn-success" type="submit" href="update.html?id=${data[i].departmentId}"><i class="fa-solid fa-circle-plus fa-3x"></i></a></td></tr>`;
+        ).innerHTML += `<tr class="text-center"><td class="table-secondary text-center py-4">${element.departmentId}</td>
+                            <td class="table-secondary text-center py-4">${element.departmentName}</td>
+                            <td class ="table-secondary text-center py-4">${element.locationId}</td>
+                            <td class="table-secondary  text-center "><button class="btn btn-warning text-light text-center" type="submit" onclick="deleteById(${data[i].departmentId})"><i class="fa-solid fa-trash-can fa-2x"></i></button>
+                            <td class="table-secondary text-center"><a class="text-success" type="submit" href="update.html?id=${data[i].departmentId}"><i class="fa-solid fa-circle-plus fa-3x"></i></a></td></tr>`;
       }
     }
 
@@ -107,14 +132,16 @@ function sendData() {
     console.log(statusData);
     if (statusData == true) {
       Swal.fire({
-        title: 'Update Data Success',
+        title: 'Adding Data Success',
         confirmButtonText: 'Save',
       }).then(function () {
         window.location = "index.html";
+        loadContent();
       });
     } else {
       Swal.fire(this.response.massage);
     }
+
   };
   xhr.send(data);
 
--- update.js
+++ update.js
@@ -1,32 +1,48 @@
 function updateData() {
-  var xhr = new XMLHttpRequest();
-  var url = "http://localhost:9091/departments/update";
-  var data = JSON.stringify({
-    departmentId: new URL(window.location).searchParams.get("id"),
-    departmentName: document.getElementById("departmentName").value,
-    managerId: document.getElementById("managerId").value,
-    locationId: document.getElementById("locationId").value,
-  });
-  xhr.open("PUT", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhr.onload = function () {
-    console.log(this.responseText);
-  };
-  xhr.onloadend = function () {
-    var statusData = JSON.parse(this.responseText).status;
-    console.log(statusData);
-    if (statusData == true) {
-      Swal.fire({
-        title: 'Insert Data Success',
-        confirmButtonText: 'Save',
-      }).then(function () {
-        window.location = "index.html";
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, update it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      var xhr = new XMLHttpRequest();
+      var url = "http://localhost:9091/departments/update";
+      var data = JSON.stringify({
+        departmentId: new URL(window.location).searchParams.get("id"),
+        departmentName: document.getElementById("departmentName").value,
+        managerId: document.getElementById("managerId").value,
+        locationId: document.getElementById("locationId").value,
       });
+      xhr.open("PUT", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.onload = function () {
+        console.log(this.responseText);
+      };
+      xhr.onloadend = function () {
+        var statusData = JSON.parse(this.responseText).status;
+        console.log(statusData);
+        if (statusData == true) {
+          Swal.fire({
+            title: 'Insert Data Success',
+            confirmButtonText: 'Save',
+          }).then(function () {
+            window.location = "index.html";
+          });
+        } else {
+          Swal.fire(this.response.massage);
+        }
+      };
+      xhr.send(data);
+      return false;
     } else {
-      Swal.fire(this.response.massage);
+      window.location = "update.html";
+      return false;
     }
-  };
-  xhr.send(data);
+  });
   return false;
 }
 
